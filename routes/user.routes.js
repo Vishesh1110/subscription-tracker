cb694119ab12b109208ca165578efb21f74f356d
Vishesh1110 <visheshvividh@gmail.com>
2026-02-08 17:28:13 +0530

    secured a request with authorize middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { getUserById, getUsers } from "../controllers/user.controller.js"
+import authorize from '../middlewares/auth.middleware.js'
 
 const userRouter = Router()
 
 userRouter.get('/', getUsers)
 
-userRouter.get('/:id', getUserById)
+userRouter.get('/:id', authorize, getUserById)
 
 userRouter.post('/', (req, res) => res.send({title: 'CREATE new user' }))
 
